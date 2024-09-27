c6042e1c056a07d32c2157bea2ea2594c87d6973
John Hinch <johnhnch864@gmail.com>
2024-09-27 17:35:16 +0100

    Tower Colour

--- games-engines-2024/scenes/wall.gd
+++ games-engines-2024/scenes/wall.gd
@@ -9,14 +9,18 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var material = StandardMaterial3D.new()
+	randomize()
+	
 	
 	for row in range(rows):
 		for col in range(cols):
-			material.albedo_color=randi_range(0,255)
 			var brick = brick_scene.instantiate()
-			
 			var pos=Vector3(cos(col)*PI,row,sin(col)*PI)
+			
+			var material = StandardMaterial3D.new()
+			
+			material.albedo_color = Color.from_hsv(randf_range(0, 1), 1, 1, 0.8)
+			brick.get_node("Brick").material_override = material
 			
 			brick.position=pos
 			add_child(brick)
