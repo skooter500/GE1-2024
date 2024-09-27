e1828c287b4445c11ac78da690e66b8fc43aeab3
John Hinch <johnhnch864@gmail.com>
2024-09-27 17:26:03 +0100

    Tower

--- games-engines-2024/scenes/wall.gd
+++ games-engines-2024/scenes/wall.gd
@@ -1,17 +1,23 @@
 extends Node3D
 
+
 @export var brick_scene:PackedScene
 @export var rows =10
-@export var cols =10
+@export var cols =100
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var material = StandardMaterial3D.new()
+	
 	for row in range(rows):
 		for col in range(cols):
+			material.albedo_color=randi_range(0,255)
 			var brick = brick_scene.instantiate()
-			var pos=Vector3(col,row,0)
+			
+			var pos=Vector3(cos(col)*PI,row,sin(col)*PI)
+			
 			brick.position=pos
 			add_child(brick)
 	
