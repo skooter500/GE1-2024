b366515527076756a514fb850172c2e9a7cc1110
lovely.fernandez@students.fhnw.ch <lovely.fernandez@students.fhnw.ch>
2024-10-17 14:39:59 +0100

    my-turret for lab 03 done, watched the lecture

--- labs/my-turret_lab02/scenes/player.gd
+++ labs/my-turret_lab02/scenes/player.gd
@@ -4,6 +4,10 @@ extends CharacterBody3D
 @export var speed:float = -1
 @export var rot_speed = 80
 
+# bullets
+@export var bullet_scene:PackedScene
+@export var bullet_spawn:Node3D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -25,11 +29,30 @@ func _physics_process(delta: float) -> void:
 	v = global_transform.basis.z
 	print(v)
 	
-	velocity = v * speed * f 
+	velocity = - v * speed * f 
 	
 	move_and_slide()
 	
 	var r = Input.get_axis("turn_left", "turn_right")
-	rotate_y(deg_to_rad(rot_speed) * r * delta)
+	rotate_y(- deg_to_rad(rot_speed) * r * delta)
+	
+	DebugDraw2D.set_text("position", position)
+	DebugDraw2D.set_text("global_position", position)
+	
+	DebugDraw2D.set_text("basis.x", transform.basis.x)
+	DebugDraw2D.set_text("basis.y", transform.basis.y)
+	DebugDraw2D.set_text("basis.z", transform.basis.z)
+
+	DebugDraw2D.set_text("glo basis.x", global_transform.basis.x)
+	DebugDraw2D.set_text("glo basis.y", global_transform.basis.y)
+	DebugDraw2D.set_text("glo basis.z", global_transform.basis.z)
+	
+	if Input.is_action_pressed("shoot"):
+		var bullet = bullet_scene.instantiate()
+		get_parent().add_child(bullet)		
+
+		bullet.global_position = bullet_spawn.global_position
+		bullet.global_rotation = bullet_spawn.global_rotation
+		
 	
 	pass
