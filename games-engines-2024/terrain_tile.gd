3ccef67f6fa54d875016d7b5d71070b7c49c5aba
skooter500 <bryan.duggan@tudublin.ie>
2024-11-16 21:47:01 +0000

    no hills

--- games-engines-2024/terrain_tile.gd
+++ games-engines-2024/terrain_tile.gd
@@ -12,21 +12,15 @@ var collision_shape: CollisionShape3D
 var current_mesh: ArrayMesh
 var t: float = 0.0
 
-# Wait until positioned in scene before generating
 func _enter_tree():
-	# Let a frame pass so our position is set
 	await get_tree().process_frame
 	generate_mesh()
 
-# Remove the mesh generation from _ready
 func _ready():
 	pass
 
 func sample_cell(row: float, col: float) -> float:
-	# Get the tile's world position
 	var world_pos = global_position
-	
-	# Add the world position to the sampling coordinates
 	var sample_x = ((col * width_scale) + world_pos.x) * perlin_scale
 	var sample_z = ((row * width_scale) + world_pos.z) * perlin_scale
 	
@@ -35,11 +29,7 @@ func sample_cell(row: float, col: float) -> float:
 	height *= abs(height)
 	height = int(height/0.5)
 	return height
-	
-	
-	#return noise_2d(sample_x, sample_z) * height_scale
-	# return sin(sample_x) * sin(sample_z) * height_scale
-	
+
 func generate_mesh():
 	mesh_instance = MeshInstance3D.new()
 	add_child(mesh_instance)
@@ -72,61 +62,45 @@ func create_mesh():
 			var tr = bottom_left + Vector3((col + 1) * width_scale, sample_cell(row + 1, col + 1), (row + 1) * width_scale)
 			var br = bottom_left + Vector3((col + 1) * width_scale, sample_cell(row, col + 1), row * width_scale)
 			
-			# Calculate normal for first triangle
+			# Calculate UVs for each vertex
+			var uv_bl = Vector2(float(col) / quads_per_tile, float(row) / quads_per_tile)
+			var uv_tl = Vector2(float(col) / quads_per_tile, float(row + 1) / quads_per_tile)
+			var uv_tr = Vector2(float(col + 1) / quads_per_tile, float(row + 1) / quads_per_tile)
+			var uv_br = Vector2(float(col + 1) / quads_per_tile, float(row) / quads_per_tile)
 			
-			st.add_vertex(br)            
-			st.add_vertex(tl)
+			# First triangle (bl, br, tl) - counter-clockwise order
+			var normal1 = (br - bl).cross(tl - bl).normalized()
+			st.set_normal(normal1)
+			st.set_uv(uv_bl)
 			st.add_vertex(bl)
 			
-			
-			# Second triangle
-			st.add_vertex(tr)
-			st.add_vertex(tl)
+			st.set_normal(normal1)
+			st.set_uv(uv_br)
 			st.add_vertex(br)
+			
+			st.set_normal(normal1)
+			st.set_uv(uv_tl)
+			st.add_vertex(tl)
+			
+			# Second triangle (br, tr, tl) - counter-clockwise order
+			var normal2 = (tr - br).cross(tl - br).normalized()
+			st.set_normal(normal2)
+			st.set_uv(uv_br)
+			st.add_vertex(br)
+			
+			st.set_normal(normal2)
+			st.set_uv(uv_tr)
+			st.add_vertex(tr)
+			
+			st.set_normal(normal2)
+			st.set_uv(uv_tl)
+			st.add_vertex(tl)
 	
-			st.generate_normals()
-	st.generate_normals()
 	st.generate_tangents()
 	current_mesh = st.commit()
 
 func noise_2d(x: float, y: float) -> float:
-	# Simple implementation of 2D noise using FastNoiseLite
 	var noise = FastNoiseLite.new()
 	noise.seed = 1234
 	noise.frequency = 1.0
 	return (noise.get_noise_2d(x, y) + 1.0) / 2.0
-
-#func _process(delta: float):
-	#var st = SurfaceTool.new()
-	#st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	#
-	#var bottom_left = Vector3(-quads_per_tile / 2.0, 0, -quads_per_tile / 2.0)
-	#
-	## Generate vertices and triangles with updated heights
-	#for row in range(quads_per_tile):
-		#for col in range(quads_per_tile):
-			#var bl = bottom_left + Vector3(col, sample_cell(row + t, col + t), row)
-			#var tl = bottom_left + Vector3(col, sample_cell(row + 1 + t, col + t), row + 1)
-			#var tr = bottom_left + Vector3(col + 1, sample_cell(row + 1 + t, col + 1 + t), row + 1)
-			#var br = bottom_left + Vector3(col + 1, sample_cell(row + t, col + 1 + t), row)
-			#
-			## First triangle (in the corrected order you found)
-			#st.add_vertex(br)
-			#st.add_vertex(tl)
-			#st.add_vertex(bl)
-			#
-			## Second triangle
-			#st.add_vertex(tr)
-			#st.add_vertex(tl)
-			#st.add_vertex(br)
-	#
-	#st.generate_normals()
-	#current_mesh = st.commit()
-	#mesh_instance.mesh = current_mesh
-	#
-	#var collision_shape_mesh = ConcavePolygonShape3D.new()
-	#collision_shape_mesh.set_faces(current_mesh.get_faces())
-	#collision_shape.shape = collision_shape_mesh
-	#
-	#t += speed * delta
-	#print(t)
